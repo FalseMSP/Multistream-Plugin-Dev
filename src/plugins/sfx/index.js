916e1fb18656422cda1478666f4ab16b9b1b861f
FalseMSP <mark.philip21@gmail.com>
2026-05-20 12:09:54 -0400

    chicken scream

--- src/plugins/sfx/index.js
+++ src/plugins/sfx/index.js
@@ -33,6 +33,7 @@ const log = require('../../logger');
 const SFX_MAP = {
   'Vine Boom':       '/sfx/vine-boom.mp3',
   'Metal Pipe':      '/sfx/metal-pipe.mp3',
+  'Chicken Scream':   '/sfx/chicken-scream.mp3',
 };
 
 const MASTER_VOLUME  = parseFloat(process.env.SFX_VOLUME       ?? '1.0');
